4bf0ac375a6db06d83228bd9ca4acb9053d9795f
Adhil Roshan <58517896+adhilroshan@users.noreply.github.com>
2024-09-05 00:53:43 +0530

    chore: Add logo link to header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,12 @@ const Header = ({ currentSlide, totalSlides }) => {
     const { darkMode } = useDarkModeStore();
     return(
     <header className="p-4 md:mx-10 flex justify-between items-center">
+        <a href="/">
         {
             darkMode? <img src={"/logo-no-background.png"} alt="Presently Logo" className="h-8" /> : <img src="/logo-black-no-background.png" alt="Presently Logo" className="h-8" />
             // :  <img src="/logo-black-no-background.png" alt="Presently Logo" className="h-8 " />
         }
+        </a>
            
         <div>Slide {currentSlide} of {totalSlides}</div>
     </header>
